c34b7f54a1c200c3bcbe7520468463fc57acb228
unknown <rlagusqks0@naver.com>
2021-11-26 12:25:45 +0900

    'step12'

---
+++ 12_albtg.tf
@@ -0,0 +1,19 @@
+resource "aws_lb_target_group" "hbkim_albtg" {
+name ="hbkim-albtg"
+port = 80
+protocol ="HTTP"
+vpc_id = aws_vpc.hbkim_vpc.id
+
+health_check {
+  enabled = true
+  healthy_threshold = 3
+  interval = 5
+  matcher = "200"
+  path = "/health.html"
+  port = "traffic-port"
+  protocol = "HTTP"
+  timeout = 2
+  unhealthy_threshold = 2
+
+}
+}
